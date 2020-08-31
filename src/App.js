eb7710a8cb7de9df9b42b08130e970cc59054d00
Claire Lesh <cteterita@gmail.com>
2020-08-31 14:17:54 -0700

    implemented routing for / and /folder/:folderId

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import './App.css';
 
 class App extends React.Component {
   state = store;
+  listView(routeProps) {
+    const { notes, folders } = this.state;
+    const folderId = routeProps.match.params.folderId;
+    return (
+      <>
+        <Sidebar folders={folders} selectedFolderId={folderId}/>
+        <NoteList notes={folderId ? notes.filter(n => n.folderId === folderId) : notes} />
+      </>
+    );
+  }
   render() {
     return (
       <>
@@ -17,6 +27,9 @@ class App extends React.Component {
         <main className='App'>
           <Sidebar folders={this.state.folders} />
           <NoteList notes={this.state.notes} />
+        <main className="App">
+         <Route exact path ="/" render={(props) => this.listView(props)} />
+         <Route exact path ="/folder/:folderId" render={(props) => this.listView(props)} />
         </main>
       </>
     );
