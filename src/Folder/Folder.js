30b7cc75c81cdbe46140977b891cf17fbb0b28c1
Claire Lesh <cteterita@gmail.com>
2020-08-31 14:17:06 -0700

    added links to Folder and NotePreview components

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Folder.css';
 
 function Folder(props) {
   return (
-    <ul className="Folder">
-      {props.name}
+    <ul className="Folder {}">
+      <Link to={`/folder/${props.id}`}>{props.name}</Link>
     </ul>
   );
 }
--- src/NotePreview/NotePreview.js
+++ src/NotePreview/NotePreview.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotePreview.css';
 
 function NotePreview(props) {
   return (
     <ul className="NotePreview">
-      {props.name} <br />
+      <Link to={`/note/${props.id}`}>{props.name}</Link> <br />
       {props.modified}
       <button className="delete-note-button">Delete</button>
     </ul>
