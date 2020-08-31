1dcbfea5ebff7678688ef869292585b8810923e1
Claire Lesh <cteterita@gmail.com>
2020-08-31 12:11:38 -0700

    added store and state and changed component to Component class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import store from './dummy-store.js'
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <header>
-        <h1><Link to="/">Noteful</Link></h1>
-      </header>
-      <main className='App'>
-        {/* content goes here */}
-      </main>
-    </>
-  );
+class App extends React.Component {
+  state = store;
+  render() {
+    return (
+      <>
+        <header>
+          <h1><Link to="/">Noteful</Link></h1>
+        </header>
+        <main className='App'>
+          {/* content goes here */}
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
