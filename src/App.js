cbdb6bcead69d9b9a11a56d628333d5601275967
Claire Lesh <cteterita@gmail.com>
2020-09-01 17:07:27 -0700

    added context provider to app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import Context from './Context';
 import Sidebar from './Sidebar/Sidebar';
 import NoteSidebar from './NoteSidebar/NoteSidebar';
 import NoteList from './NoteList/NoteList';
@@ -9,12 +10,16 @@ import './App.css';
 
 class App extends React.Component {
   state = store;
+  deleteNote(noteId) {
+    //To implement
+    console.log(`deleting note ${noteId}`);
+  }
   listView(routeProps) {
     const { notes, folders } = this.state;
     const folderId = routeProps.match.params.folderId;
     return (
       <>
-        <Sidebar folders={folders} selectedFolderId={folderId}/>
+        <Sidebar folders={folders} selectedFolderId={folderId} />
         <NoteList notes={folderId ? notes.filter(n => n.folderId === folderId) : notes} />
       </>
     );
@@ -33,15 +38,21 @@ class App extends React.Component {
     );
   }
   render() {
+    const contextValue = {
+      ...store,
+      deleteNote: this.deleteNote
+    }
     return (
       <>
         <header>
           <h1><Link to="/">Noteful</Link></h1>
         </header>
         <main className="App">
-         <Route exact path ="/" render={(props) => this.listView(props)} />
-         <Route exact path ="/folder/:folderId" render={(props) => this.listView(props)} />
-         <Route exact path ="/note/:noteId" render={(props) => this.noteView(props)} />
+          <Context.Provider value={contextValue}>
+          <Route exact path ="/" render={(props) => this.listView(props)} />
+          <Route exact path ="/folder/:folderId" render={(props) => this.listView(props)} />
+          <Route exact path ="/note/:noteId" render={(props) => this.noteView(props)} />
+          </Context.Provider>
         </main>
       </>
     );
