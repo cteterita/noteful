0328670c6561f071323a5b3f01c05673ebe78d37
Claire Lesh <cteterita@gmail.com>
2020-09-05 16:12:57 -0700

    rewrite NotePreview as stateless class function

--- src/NotePreview/NotePreview.js
+++ src/NotePreview/NotePreview.js
@@ -4,33 +4,35 @@ import PropTypes from 'prop-types';
 import './NotePreview.css';
 import Context from '../Context';
 
-class NotePreview extends React.Component {
-  static contextType = Context;
-
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    modified: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { id, name, modified } = this.props;
-    const { deleteNote } = this.context;
-    return (
-      <ul className="NotePreview">
-        <Link to={`/note/${id}`}>{name}</Link>
-        <br />
-        {modified}
-        <button
-          className="delete-note-button"
-          onClick={() => deleteNote(id)}
-          type="submit"
-        >
-          Delete
-        </button>
-      </ul>
-    );
-  }
+function NotePreview(props) {
+  return (
+    <Context.Consumer>
+      {(context) => {
+        const { id, name, modified } = props;
+        const { deleteNote } = context;
+        return (
+          <ul className="NotePreview">
+            <Link to={`/note/${id}`}>{name}</Link>
+            <br />
+            {modified}
+            <button
+              className="delete-note-button"
+              onClick={() => deleteNote(id)}
+              type="submit"
+            >
+              Delete
+            </button>
+          </ul>
+        );
+      }}
+    </Context.Consumer>
+  );
 }
+
+NotePreview.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  modified: PropTypes.string.isRequired,
+};
 
 export default NotePreview;
